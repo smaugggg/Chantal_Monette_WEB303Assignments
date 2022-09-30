61f524638b2a6a2c8d73df532d742b8d2cbc0d59
smaugggg <roborant@gmail.com>
2022-09-29 20:46:38 -0400

    attempting delay with .ajax function

--- js/script.js
+++ js/script.js
@@ -2,48 +2,54 @@
 // Chantal Monette W0800871
 // started20220929 
 
-/* let getTeam = $.getJSON("../team.json").done(function(data) {
-    $.each(data.team, function(index, value) {
-        let teamName = $("<h2></h2>").text(value.name);
-        let teamPosition = $("<h5></h5>").text(value.position);
-        let teamBio = $("<p></p>").text(value.bio);
-
-        $("div#team").append(teamName);
-        $("div#team").append(teamPosition);
-        $("div#team").append(teamBio);
-    });
-});
-
-let getTeam2 = $.getJSON("../team.json").done(function(data) {
-    $.each(data.team, function(index, value) {
-        $("#team").append("<h2>${value.name}</h2><br /><h5>${value.position}</h5><br /><p>${value.bio}</p><br />"); 
-    });
-}); */
-
-
-let teamInfo = $.ajax({
-    type: 'GET',
-    url: '../team.json',
-    datatype: 'JSON',
-    beforeSend: function() {
-        $("#team").append("Loading...");
-    },
-    error: function(xhr, error) {
-        $("#team").append("There was an error in retrieving the data.");
-    },
-    success: function(data) {
+function getTeamJSON() {
+    $.getJSON("../team.json").done(function(data) {
         $.each(data.team, function(index, value) {
             let teamName = $("<h2></h2>").text(value.name);
             let teamPosition = $("<h5></h5>").text(value.position);
             let teamBio = $("<p></p>").text(value.bio);
-    
-            $("#team").append(teamName, teamPosition, teamBio);
-        });
-    },
-});
 
+            $("div#team").append(teamName);
+            $("div#team").append(teamPosition);
+            $("div#team").append(teamBio);
+        });
+    });
+}
+
+function getTeamJSON2() { 
+    $.getJSON("../team.json").done(function(data) {
+        $.each(data.team, function(index, value) {
+            $("#team").append("<h2>${value.name}</h2><br /><h5>${value.position}</h5><br /><p>${value.bio}</p><br />"); 
+        });
+    }); 
+}
+
+
+function getTeamAJAX() {
+    $.ajax({
+        type: 'GET',
+        url: '../team.json',
+        datatype: 'JSON',
+        beforeSend: function() {
+            $("#team").append("Loading...");
+        },
+        error: function(xhr, error) {
+            $("#team").append("There was an error in retrieving the data.");
+        },
+        success: function(data) {
+            $.each(data.team, function(index, value) {
+                let teamName = $("<h2></h2>").text(value.name);
+                let teamPosition = $("<h5></h5>").text(value.position);
+                let teamBio = $("<p></p>").text(value.bio);
+        
+                $("#team").append(teamName, teamPosition, teamBio);
+            });
+        },
+    });
+}
 
 $(document).ready(function() {
-
+    let callDelay = 3000;
+    setTimeout(getTeamAJAX(), callDelay);
 });
 
