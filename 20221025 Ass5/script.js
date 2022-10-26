43a5417b049e6014224c579134c80b52be672285
smaugggg <roborant@gmail.com>
2022-10-26 05:32:28 -0400

    the buttons are not working local, check sandbox

--- 20221025 Ass5/script.js
+++ 20221025 Ass5/script.js
@@ -17,10 +17,17 @@ $(document).ready(function () {
         }
         
         updateContentItem(id, name, desc, genre) {
-            if(id = ContentItem.id && (name != null || desc != null || genre !=null)) {
+            if(id === this.id && (name !== null || desc !== null || genre !== null)) {
+                this.id = id;
                 this.name = name;
                 this.desc = desc;
                 this.genre = genre;
+
+                $('content-item-' + this.id).innerHTML = "<div class='content-item-wrapper' id='content-item-" + this.id + "'>" +
+                                                         "<h2>" + this.name + "</h2>" + 
+                                                         "<p>" + this.desc + "</p>" +
+                                                         "<div>" + this.genre + "</div>" +
+                                                         "</div>";
             }
         }
 
@@ -33,11 +40,11 @@ $(document).ready(function () {
         }
     }
 
-    let kpopgroups = [new ContentItem('0', 'Dreamcatcher', 'This is a girl group formed by HappyFace Entertainment (now Dreamcatcher Company). The group consists of seven members: JiU, SuA, Siyeon, Handong, Yoohyeon, Dami and Gahyeon. They made their official debut on January 13, 2017, with the single album Nightmare.', 'Rock'),
-                      new ContentItem('1', 'Stray Kids', 'This is a boy band formed by JYP Entertainment through a 2017 reality show of the same name. The group is composed of eight members: Bang Chan, Lee Know, Changbin, Hyunjin, Han, Felix, Seungmin, and I.N. Originally a nine-piece group, member Woojin left due to undisclosed personal reasons in October 2019. Stray Kids released their pre-debut extended play (EP) Mixtape in January 2018 and officially debuted on March 25 with the EP I Am Not.', 'Electronica'),
-                      new ContentItem('2', '(G)I-dle', 'This is a girl group formed by Cube Entertainment. The group consists of five members: Miyeon, Minnie, Soyeon, Yuqi and Shuhua. Originally a sextet, Soojin left the lineup on August 14, 2021. (G)I-dle debuted on May 2, 2018, with the single "Latata" from their first extended play (EP) I Am.', 'Hip Hop'),
-                      new ContentItem('3', 'IVE', 'This is a girl group formed by Starship Entertainment. The group is composed of six members: Yujin, Gaeul, Rei, Wonyoung, Liz and Leeseo. They made their debut on December 1, 2021, with the single album Eleven', 'Pop'),
-                      new ContentItem('4', 'STAYC', 'This is a girl group formed by High Up Entertainment. The group is composed of six members: Sumin, Sieun, Isa, Seeun, Yoon, and J. They debuted on November 12, 2020, with the release of their debut single album Star to a Young Culture.', 'Pop'),
+    let kpopgroups = [new ContentItem(0, 'Dreamcatcher', 'This is a girl group formed by HappyFace Entertainment (now Dreamcatcher Company). The group consists of seven members: JiU, SuA, Siyeon, Handong, Yoohyeon, Dami and Gahyeon. They made their official debut on January 13, 2017, with the single album Nightmare.', 'Rock'),
+                      new ContentItem(1, 'Stray Kids', 'This is a boy band formed by JYP Entertainment through a 2017 reality show of the same name. The group is composed of eight members: Bang Chan, Lee Know, Changbin, Hyunjin, Han, Felix, Seungmin, and I.N. Originally a nine-piece group, member Woojin left due to undisclosed personal reasons in October 2019. Stray Kids released their pre-debut extended play (EP) Mixtape in January 2018 and officially debuted on March 25 with the EP I Am Not.', 'Electronica'),
+                      new ContentItem(2, '(G)I-dle', 'This is a girl group formed by Cube Entertainment. The group consists of five members: Miyeon, Minnie, Soyeon, Yuqi and Shuhua. Originally a sextet, Soojin left the lineup on August 14, 2021. (G)I-dle debuted on May 2, 2018, with the single "Latata" from their first extended play (EP) I Am.', 'Hip Hop'),
+                      new ContentItem(3, 'IVE', 'This is a girl group formed by Starship Entertainment. The group is composed of six members: Yujin, Gaeul, Rei, Wonyoung, Liz and Leeseo. They made their debut on December 1, 2021, with the single album Eleven', 'Pop'),
+                      new ContentItem(4, 'STAYC', 'This is a girl group formed by High Up Entertainment. The group is composed of six members: Sumin, Sieun, Isa, Seeun, Yoon, and J. They debuted on November 12, 2020, with the release of their debut single album Star to a Young Culture.', 'Pop'),
                      ];
     
     kpopgroups.forEach(ContentItem => {
@@ -52,7 +59,6 @@ $(document).ready(function () {
 
 
     // Bonus stuff. I can get the buttons to appear but not to work. need to try again later.
-
     let updateSucceed = document.createElement('button');
         updateSucceed.classList.add("success");
         updateSucceed.textContent = 'Successfully Update';
@@ -60,10 +66,16 @@ $(document).ready(function () {
         updateFail.classList.add("failure");
         updateFail.textContent = 'Try to Update';
 
-    //$("#container").append(updateSucceed, updateFail);
+    $("#content-item-list").append(updateSucceed, updateFail);
 
-    updateSucceed.addEventListener('click', event => {
-        updateContentItem('1', 'BlackPink', 'This is a girl group formed by YG Entertainment. The group consists of four members Jisoo, Jennie, Rose, and Lisa. They debuted in August 2016 with their single album Square One, featuring the hit single Boombayah.', 'EDM');
+    $('.success').on('click', function() {
+        kpopgroups[1].updateContentItem(1, 'BlackPink', 'This is a girl group formed by YG Entertainment. The group consists of four members Jisoo, Jennie, Rose, and Lisa. They debuted in August 2016 with their single album Square One, featuring the hit single Boombayah.', 'EDM');
+        console.log("Content should have been updated.");
+    });
+
+    $('.failure').on('click', function() {
+        kpopgroups[1].updateContentItem(5, 'BlackPink', 'This is a girl group formed by YG Entertainment. The group consists of four members Jisoo, Jennie, Rose, and Lisa. They debuted in August 2016 with their single album Square One, featuring the hit single Boombayah.', 'EDM');
+        console.log("Failure.");
     });
 
 });
