333181caa6f793da8c018b30a70cc3a9ff19e5d4
smaugggg <roborant@gmail.com>
2022-11-17 00:12:35 -0500

    working, still need design

--- WEB303As08/js/script.js
+++ WEB303As08/js/script.js
@@ -2,6 +2,45 @@
 // Chantal Monette W0800871
 // 2022 11 17
 
+$(document).ready(function() {
+    getCharacters();
+
+    $("#am").click(function(){
+    
+        $("tbody").find("tr").show();
+        $("tbody tr").each(function() {
+            let value = $(this).children().first().text();
+            if(!value[1].match(/[a-mA-M]/)) {
+                $(this).hide();
+            }
+        });
+    });
+    $("#nz").click(function(){
+        
+        $("tbody").find("tr").show();
+        $("tbody tr").each(function() {
+            let value = $(this).children().first().text();
+            if(!value[1].match(/[n-zN-Z]/)) {
+                $(this).hide();
+            }
+        });
+    });
+
+    $("input").keyup(function () {
+        let query = this.value.toLowerCase();    
+        $('tbody tr').each(function () {
+        let $name = $(this);
+        let tname = $name.text();
+                if (tname.toLowerCase().indexOf(query) == 0) {
+                    $name.removeClass('active');
+                } else if(tname.toLowerCase().indexOf(query) != -1) {
+                    $name.addClass('active');   
+                }
+        });
+    });
+});  
+
+
 function getCharacters() {
     $.getJSON("wentworth.json").done(function(data) {
         $.each(data.inmates, function(index, value) {
@@ -14,41 +53,8 @@ function getCharacters() {
                 <td>${value.sentence}</td>
                 </tr>`
             );
-        });
-            
+        });  
     });
 }
 
-
-$(document).ready(function() {
-    getCharacters();
-});  
-
-
-/* 
-        $("#am").click(function(){
-            
-            $("tbody").find("tr").show();
-            $("tbody tr").each(function() {
-                let value = $(this).children().first().text();
-                console.log(value);
-                console.log(value[0]);
-                if(!value[0].match(/[a-mA-M]/)) {
-                    $(this).hide();
-                }
-            });
-        });
-        $("#nz").click(function(){
-            
-            $("tbody").find("tr").show();
-            $("tbody tr").each(function() {
-                let value = $(this).children().first().text();
-                console.log(value);
-                console.log(value[0]);
-                if(!value[0].match(/[n-zN-Z]/)) {
-                    $(this).hide();
-                }
-            });
-        });
-    }); */
 
