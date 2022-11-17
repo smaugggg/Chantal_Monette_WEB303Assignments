7010246555439ac507c1d731adeb68b17487b7b5
smaugggg <roborant@gmail.com>
2022-11-16 23:24:17 -0500

    hello?

---
+++ WEB303As08/js/script.js
@@ -0,0 +1,53 @@
+// WEB303 Assignment 8
+// Chantal Monette W0800871
+// 2022 11 17
+
+function getCharacters() {
+    $.getJSON("wentworth.json").done(function(data) {
+        $.each(data.inmates, function(index, value) {
+            
+            $('.inmate').append(
+                `<tr>
+                <td class="name">${value.firstname + " " + value.lastname}</td>
+                <td>${value.nickname}</td>
+                <td>${value.occupation}</td>
+                <td>${value.alive}</td>
+                <td>${value.sentence}</td>
+                </tr>`);
+            });
+    
+    });
+}
+
+$(document).ready(function() {
+    getCharacters();
+});  
+
+
+/* 
+        $("#am").click(function(){
+            
+            $("tbody").find("tr").show();
+            $("tbody tr").each(function() {
+                let value = $(this).children().first().text();
+                console.log(value);
+                console.log(value[0]);
+                if(!value[0].match(/[a-mA-M]/)) {
+                    $(this).hide();
+                }
+            });
+        });
+        $("#nz").click(function(){
+            
+            $("tbody").find("tr").show();
+            $("tbody tr").each(function() {
+                let value = $(this).children().first().text();
+                console.log(value);
+                console.log(value[0]);
+                if(!value[0].match(/[n-zN-Z]/)) {
+                    $(this).hide();
+                }
+            });
+        });
+    }); */
+
